218a173809dbd167f5f6fb7e8cf125257caad729
Tim Golla <tim.golla.official@gmail.com>
2021-02-06 12:38:29 +0100

    improve code formatting

--- NearestNeighborFunctions.py
+++ NearestNeighborFunctions.py
@@ -5,18 +5,22 @@ import numpy as np
 import scipy.sparse as sp
 from sklearn.neighbors import NearestNeighbors
 
-def getKNNGraphsklearn(m1,m2, n_neighbors, algorithm='auto',n_jobs=-1, metric='euclidean'):
+
+def getKNNGraphsklearn(m1, m2, n_neighbors, algorithm='auto', n_jobs=-1, metric='euclidean'):
     st = time.time()
-    n_neighbors=min(n_neighbors,len(m2))
-    nbrs12 = NearestNeighbors(n_neighbors, algorithm='auto',n_jobs=-1, metric='euclidean').fit(m2)
-    dists,nninds=  nbrs12.kneighbors(m1)
-    dists += 1e-7 # wiggle the dists a bit, so the sparse matrix representation is the way we need it
+    n_neighbors = min(n_neighbors, len(m2))
+    nbrs12 = NearestNeighbors(
+        n_neighbors, algorithm='auto', n_jobs=-1, metric='euclidean').fit(m2)
+    dists, nninds = nbrs12.kneighbors(m1)
+    dists += 1e-7  # wiggle the dists a bit, so the sparse matrix representation is the way we need it
     i = np.arange(dists.shape[0])
-    i = np.repeat(i,n_neighbors)
-    costmatrix1 = sp.coo_matrix((dists.flatten('C'),(i,nninds.flatten('C'))), shape = (len(m1), len(m2)))
+    i = np.repeat(i, n_neighbors)
+    costmatrix1 = sp.coo_matrix(
+        (dists.flatten('C'), (i, nninds.flatten('C'))), shape=(len(m1), len(m2)))
     et = time.time()
     totaltime = et - st
     return costmatrix1, totaltime
 
-def getKNNGraph(m1,m2, n_neighbors):
-    return getKNNGraphsklearn(m1,m2, n_neighbors, algorithm='auto',n_jobs=-1, metric='euclidean')
+
+def getKNNGraph(m1, m2, n_neighbors):
+    return getKNNGraphsklearn(m1, m2, n_neighbors, algorithm='auto', n_jobs=-1, metric='euclidean')
--- OptimalTransport.py
+++ OptimalTransport.py
@@ -8,14 +8,15 @@ import matplotlib.pyplot as plt
 import warnings
 import time
 
-def sinkhorn_sparse(K, s, d, epsilon=1e-5, maxiter = 1000, verbose = False, plot=False):
+
+def sinkhorn_sparse(K, s, d, epsilon=1e-5, maxiter=1000, verbose=False, plot=False):
     st = time.time()
-    s /=s.sum()
-    d /=d.sum()
+    s /= s.sum()
+    d /= d.sum()
     u = np.ones(s.shape)
     v = np.ones(d.shape)
-    assert(np.isclose(np.sum(s),1))
-    assert(np.isclose(np.sum(d),1))
+    assert(np.isclose(np.sum(s), 1))
+    assert(np.isclose(np.sum(d), 1))
     difference_norm = 2*epsilon
     it = 0
     plotstep = 100
@@ -26,36 +27,37 @@ def sinkhorn_sparse(K, s, d, epsilon=1e-5, maxiter = 1000, verbose = False, plot
             u = s / K.dot(v)
             v = d / K.T.dot(u)
         except FloatingPointError:
-            warnings.warn("Floating point error. Using last working tranport matrix and returning")
+            warnings.warn(
+                "Floating point error. Using last working tranport matrix and returning")
             break
         if sp.issparse(K):
-            T = (K.multiply(v[None,:])).multiply( u[:,None])
+            T = (K.multiply(v[None, :])).multiply(u[:, None])
             difference_norm = spl.norm(T - T_previous)
             if not np.all(np.isfinite(T.data)):
                 T = T_previous
                 print("The transport matrix has nans. Using last version and breaking.")
                 break
         else:
-            T = u[:,None]*K*v[None,:]
+            T = u[:, None]*K*v[None, :]
             difference_norm = npl.norm(T - T_previous)
         T_previous = T
         if verbose:
             print("difference norm = " + str(difference_norm))
         if plot:
-            if it%plotstep == 0:
+            if it % plotstep == 0:
                 if sp.issparse(K):
                     im = plt.spy(T)
                 else:
                     im = plt.imshow(T)
                 plt.savefig("j:/temp/" + str(it) + ".png")
                 plt.close()
-        it+=1
+        it += 1
     if verbose:
         print("Done. Constructing transport matrix")
     T_sum = T.sum()
-    if not np.isclose(T_sum,1):
+    if not np.isclose(T_sum, 1):
         warnings.warn("Sum transport matrix (should be 1): " + str(T_sum))
-        T/= T_sum
+        T /= T_sum
     if plot:
         if sp.issparse(K):
             plt.spy(T)
@@ -66,4 +68,3 @@ def sinkhorn_sparse(K, s, d, epsilon=1e-5, maxiter = 1000, verbose = False, plot
     et = time.time()
     totaltime = et - st
     return T, totaltime
-
